a9178fa791ec6220a64c4eb7481e4f7346ec02b3
Arthur Llorens <arthurllorens@hotmail.fr>
2021-12-15 12:21:46 +0100

    inital commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+string[] days = { "Lundi", "Mardi", "Mercredi", "Jedi", "Vendredi", "Samedi"};
+Console.WriteLine(days[1]);
+Console.WriteLine(days[4]);
+Console.WriteLine(days[0]);
+Console.WriteLine(days[3]);
+days[3] = "Jeudi";
+Console.WriteLine("\n");
+Array.Resize(ref days, days.Length + 1);
+days[days.Length - 1] = "Dimanche";
+for (int i = 0; i < days.Length; i++)
+{
+    Console.WriteLine(days[i]);
+}
+
