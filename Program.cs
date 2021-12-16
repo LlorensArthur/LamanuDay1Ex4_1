53fbc41092255363803aab45a0debeb8978e59ba
Arthur Llorens <arthurllorens@hotmail.fr>
2021-12-16 09:03:48 +0100

    Rendu le programme plus lisible pour l'utilisateur.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
 ﻿string[] days = { "Lundi", "Mardi", "Mercredi", "Jedi", "Vendredi", "Samedi"};
-Console.WriteLine(days[1]);
-Console.WriteLine(days[4]);
-Console.WriteLine(days[0]);
-Console.WriteLine(days[3]);
+Console.WriteLine($"Voici le contenu de l'indice 1 du tableau {days[1]}");
+Console.WriteLine($"Voici le contenu de l'indice 4 du tableau {days[4]}");
+Console.WriteLine($"Voici le premier élément du tableau {days[0]}");
+Console.WriteLine($"Voici le quatrième élément du tableau {days[3]}");
 days[3] = "Jeudi";
-Console.WriteLine("\n");
 Array.Resize(ref days, days.Length + 1);
 days[days.Length - 1] = "Dimanche";
+Console.WriteLine("\nNous allons maintenant afficher la totalité du tableau. Vous noterez que la faute d'orthographe sur le mot jeudi a été corrigé.");
 for (int i = 0; i < days.Length; i++)
 {
     Console.WriteLine(days[i]);
